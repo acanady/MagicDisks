b2404a58b1b6be16d8c69bc05f994edfa34b2476
Sadler <bulsj-19@rhodes.edu>
2019-02-19 16:14:07 -0600

    sdfsd
    dsfs

--- Assets/Scripts/DiscHandler.cs
+++ Assets/Scripts/DiscHandler.cs
@@ -11,24 +11,23 @@ using UnityEngine;
  * on the disc. */
 public class DiscHandler : MonoBehaviour
 {
-    
+    public struct signal
+    {
+        public int element; //Type of element
+        public int units; //The amount of element being passed through
+        public int inout; //0 for input 1 for output
+
+        public signal(int elem, int uni, int ino)
+        {
+            element = elem;
+            units = uni;
+            inout = ino;
+
+        }
+    }
 
     public class Tile
     {
-        public struct signal
-        {
-            public int element; //Type of element
-            public int units; //The amount of element being passed through
-            public int inout; //0 for input 1 for output
-
-            public signal(int elem, int uni, int ino)
-            {
-                element = elem;
-                units = uni;
-                inout = ino;
-
-            }
-        }
 
         //The Tile uses data from the signal struct in order to create the tile
 
@@ -81,7 +80,7 @@ public class DiscHandler : MonoBehaviour
         //Creates a Tile type from the Tile class
         public Tile mytile = new Tile();
     }
-    //End of Tile Class
+
 
 
     // All tiles and discs will have a single script on them that gives ID
@@ -89,7 +88,7 @@ public class DiscHandler : MonoBehaviour
     // Width/Height of Tiles, used for scaling the math
     float TileSize;
 
-    // Dict to hold all discs based on a specific ID
+    // Dict to hold all discs
     Dictionary<int, Disc> Discs = new Dictionary<int, Disc>();
 
     //Dict to hold all tiles based on a specific ID
@@ -243,10 +242,9 @@ public class DiscHandler : MonoBehaviour
         return arPos;
     }
 
-    Vector3 GetCellCenter(Vector2 index, Transform discTrans)
+    Vector3 GetCellCenter(Vector2 index)
     {
-        Vector3 vector = new Vector3(0, 0, 0);
-        return vector;
+        
     }
 
     // RemoveTile: Disconnects a Tile from the board, 
@@ -272,9 +270,173 @@ public class DiscHandler : MonoBehaviour
     {
         // temporarily make the tile a child of the disc to get local pos
         tile.GetComponent<Transform>().SetParent(disc.transform);
+        tile.GetComponent<Rigidbody>().isKinematic = true;
+        tile.GetComponent<Rigidbody>().useGravity = false;
+
         Vector2 pos = GetTileIndex(tile, disc);
 
+        // Check if that cell is full
+        // if it is, don't place the tile, break
+
+        Vector3 dist = GetCellCenter(pos);
+
+        tile.GetComponent<Transform>().localPosition = dist;
+        tile.GetComponent<Transform>().rotation = disc.transform.rotation;
     }
 
 
 }
+
+    public class Disc
+    {
+        public int ID;
+
+    }
+
+    // Dict to hold all discs based on a specific ID
+        int x = Mathf.RoundToInt(index.x);
+        int z = Mathf.RoundToInt(index.y);
+
+        float h = 0f;
+        float w = 0f;
+
+        switch (x)
+        {
+            case 0:
+                h = 8 * (TileSize / 9);
+                break;
+            case 1:
+                h = 6 * (TileSize / 9);
+                break;
+            case 2:
+                h = 4 * (TileSize / 9);
+                break;
+            case 3:
+                h = 2 * (TileSize / 9);
+                break;
+            case 4:
+                h = 0f;
+                break;
+            case 5:
+                h = -2 * (TileSize / 9);
+                break;
+            case 6:
+                h = -4 * (TileSize / 9);
+                break;
+            case 7:
+                h = -6 * (TileSize / 9);
+                break;
+            case 8:
+                h = -8 * (TileSize / 9);
+                break;
+        }
+
+        switch (z)
+        {
+            case 0:
+                w = 8 * (TileSize / 9);
+                break;
+            case 1:
+                w = 6 * (TileSize / 9);
+                break;
+            case 2:
+                w = 4 * (TileSize / 9);
+                break;
+            case 3:
+                w = 2 * (TileSize / 9);
+                break;
+            case 4:
+                w = 0f;
+                break;
+            case 5:
+                w = -2 * (TileSize / 9);
+                break;
+            case 6:
+                w = -4 * (TileSize / 9);
+                break;
+            case 7:
+                w = -6 * (TileSize / 9);
+                break;
+            case 8:
+                w = -8 * (TileSize / 9);
+                break;
+        }
+
+        Vector3 dist = new Vector3(h, 0f, w);
+        return dist;
+    
+
+    public class Tile
+    {
+        public struct signal
+        {
+            public int element; //Type of element
+            public int units; //The amount of element being passed through
+            public int inout; //0 for input 1 for output
+
+            public signal(int elem, int uni, int ino)
+            {
+                element = elem;
+                units = uni;
+                inout = ino;
+
+            }
+        }
+
+        //The Tile uses data from the signal struct in order to create the tile
+
+        public signal signal_data;
+        public int ID;
+        public Dictionary<string, signal> sides;
+
+
+
+        public Tile(Dictionary<string, signal> side)
+        {
+
+            sides = side;
+        }
+
+        public Tile()
+        { //Default constructor for the tile 
+            signal_data = new signal(0, 0, 0);
+            sides = new Dictionary<string, signal>
+        {
+            {"up",signal_data },
+            { "down",signal_data },
+            { "left",signal_data},
+            { "right",signal_data},
+        };
+
+        }
+
+
+        public void change_signal(signal new_signal, string side) //Changes the signal of the tile in that specific section 
+        {
+            sides["side"] = new_signal;
+        }
+
+        public void print_signal(signal p_signal)
+        {
+            Debug.Log("element: " + p_signal.element);
+            Debug.Log("units: " + p_signal.units);
+            Debug.Log("I/O: " + p_signal.inout);
+        }
+
+        public void print_tile() //Prints out the data in the tile by printing the dictionary
+        {
+            foreach (KeyValuePair<string, signal> elem in sides)
+            {
+                Debug.Log("Side:" + elem.Key);
+                print_signal(elem.Value);
+            }
+        }
+        //Creates a Tile type from the Tile class
+        public Tile mytile = new Tile();
+    }
+    //End of Tile Class
+
+
+    // All tiles and discs will have a single script on them that gives ID
+
+    // Width/Height of Tiles, used for scaling the math
